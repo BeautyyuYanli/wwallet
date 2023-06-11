04d4c96cacb0b7bacc0c91d40ec86e4d4807fdd7
BeautyyuYanli <beautyyuyanli@gmail.com>
2023-06-11 23:36:31 +0800

    fix conection

--- server/connection.ts
+++ server/connection.ts
@@ -4,7 +4,7 @@ export default createPool({
     port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'test',
+    database: process.env.DB_DATABASE || 'test',
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true
